e7547f39797ae0e6829a9ef11ec9887a5385cefc
sujanthan02 <sujanthan006@gmail.com>
2019-06-20 15:17:30 +0530

    Front Page Design

--- src/components/ProjectComponent/Model.js
+++ src/components/ProjectComponent/Model.js
@@ -1,12 +1,20 @@
-import { Modal, Button, Form, Row, Col, Input, Checkbox} from 'antd';
+import { Modal, Button, Form, Row, Col, Input, Checkbox, DatePicker, TimePicker } from 'antd';
 import React from 'react';
+import react, { Component } from 'react'
+import axios from 'axios'
+
+class home extends Component {
+
+  comp
+}
+
 
 function onChange(e) {
   console.log(`checked = ${e.target.checked}`);
 }
 export default class Model extends React.Component {
   state = {
-   
+
     disabled: true,
     visible: false,
   };
@@ -44,64 +52,131 @@ export default class Model extends React.Component {
   };
 
   render() {
+
     return (
       <div>
         <Button type="primary" onClick={this.showModal}>
-          Add Project 
+          Add Project
         </Button>
-        <br/>
+        <br />
         <Modal
           title="Add Project"
           visible={this.state.visible}
           onOk={this.handleOk}
           onCancel={this.handleCancel}
+          width="600px"
+
         >
-  <Form layout="vertical">
-<Row>
-    
-    <Col  span={24} style={{padding: "5px"}}>
-        <Form.Item label="Project Name">
-        <Input placeholder="ProjectName"/>
-         </Form.Item>{" "}
-    </Col>
-    </Row> 
-    <Row>
-    <Col>
-    <div>
-        
-        <p>
-          
-          <Checkbox
-            style={{ marginLeft: '10px' }}
-           
-            onClick={this.toggleDisable}
-            onChange={onChange}
+          <Form layout="vertical">
+            <Row>
 
-          >Edit
-            {!this.state.disabled }
-          </Checkbox>
-        
-        </p>
+              <Col span={24} style={{ padding: "5px" }}>
+                <Form.Item label="Project Name">
+                  <Input placeholder="ProjectName" />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
+
+            <Row>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Type">
+                  <Input placeholder="Type" />
+                </Form.Item>{" "}
+              </Col>
+
+
+
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Start Date" style={{ marginBottom: 0 }}>
+
+                  <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}></span>
+                  <Form.Item style={{ display: 'inline-block', width: 'calc(90% - 12px)' }}>
+                    <DatePicker />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+
+
+
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="End Date" style={{ marginBottom: 0 }}>
+
+                  <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}></span>
+                  <Form.Item style={{ display: 'inline-block', width: 'calc(90% - 12px)' }}>
+                    <DatePicker />
+                  </Form.Item>
+                </Form.Item>
+              </Col>
+
+
+            </Row>
+
+            <Row>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Duration">
+                  <Input placeholder="Duration" />
+                </Form.Item>{" "}
+              </Col>
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Status">
+                  <Input placeholder="Status" />
+                </Form.Item>{" "}
+              </Col>
+
+
+              <Col span={8} style={{ padding: "5px" }}>
+                <Form.Item label="Config Id">
+                  <Input placeholder="ConfigId" />
+                </Form.Item>{" "}
+              </Col>
+            </Row>
+
+
+
+
+
+            <Row>
+              <Col>
+                <div>
+
+                  <p>
+
+                    <Checkbox
+                      style={{ marginLeft: '10px' }}
+
+                      onClick={this.toggleDisable}
+                      onChange={onChange}
+
+                    >Edit
+            {!this.state.disabled}
+                    </Checkbox>
+
+                  </p>
+                </div>
+              </Col>
+
+
+              <Col span={24} style={{ padding: "5px" }}>
+
+                <Form.Item>
+                  <Input
+                    disabled={this.state.disabled}
+                    onChange={this.onChange}
+                    placeholder="Abbrevation" />
+                </Form.Item>{" "}
+              </Col>
+
+            </Row>
+
+          </Form>
+
+        </Modal>
       </div>
-    </Col>
-  
-    
-    <Col  span={24} style={{padding: "5px"}}>
-    
-        <Form.Item>
-        <Input 
-        disabled={this.state.disabled}
-        onChange={this.onChange}
-        placeholder="Abbrevation"   />
-         </Form.Item>{" "}
-    </Col>
-
-   </Row>
-  
-      </Form>
-      
-     </Modal>
-     </div>
     );
   }
 }
