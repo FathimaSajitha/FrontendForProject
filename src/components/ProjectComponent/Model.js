f4bae1f3253773b19a8e076e708b72bbda793e5f
jakeerththana <jakeerththana1994@gmail.com>
2019-06-21 15:51:28 +0530

    frontend delete page

--- src/components/ProjectComponent/Model.js
+++ src/components/ProjectComponent/Model.js
@@ -1,7 +1,17 @@
-import { Modal, Button, Form, Row, Col, Input, Checkbox, DatePicker, TimePicker } from 'antd';
-import React from 'react';
-import react, { Component } from 'react';
-import axios from 'axios';
+import {
+  Modal,
+  Button,
+  Form,
+  Row,
+  Col,
+  Input,
+  Checkbox,
+  DatePicker,
+  TimePicker
+} from "antd";
+import React from "react";
+import react, { Component } from "react";
+import axios from "axios";
 
 function onChange(e) {
   console.log(`checked = ${e.target.checked}`);
@@ -10,15 +20,15 @@ export default class Model extends React.Component {
   constructor(props) {
     super(props);
 
-
     this.setState({
-      projectName: '',
-      type: '',
-      startDate: '',
-      endDate: '',
-      duration: '',
-      status: '',
-      configId: '',
+      projectName: "",
+      type: "",
+      startDate: "",
+      endDate: "",
+
+      duration: "",
+      status: "",
+      configId: "",
       visible: false
     });
     this.onChangeprojectName = this.onChangeprojectName.bind(this);
@@ -28,9 +38,7 @@ export default class Model extends React.Component {
     this.onChangeDuration = this.onChangeDuration.bind(this);
     this.onChangeStatus = this.onChangeStatus.bind(this);
     this.onChangeconfigId = this.onChangeconfigId.bind(this);
-
-  };
-
+  }
 
   onChangeprojectName(e) {
     this.setState({
@@ -40,22 +48,20 @@ export default class Model extends React.Component {
   onChangeType(e) {
     this.setState({
       type: e.target.value
-    })
+    });
   }
   onChangeStartDate(date, dateString) {
     //this.setState({startDate: dateString});
-    
-    this.setState(
-      { startDate: dateString },
-      () => console.log(this.state.startDate)
+
+    this.setState({ startDate: dateString }, () =>
+      console.log(this.state.startDate)
     );
 
     console.log(this.state.startDate);
   }
   onChangeEndDate(date, dateString) {
-    this.setState(
-      { endDate: dateString },
-      () => console.log(this.state.endDate)
+    this.setState({ endDate: dateString }, () =>
+      console.log(this.state.endDate)
     );
 
     console.log(this.state.endDate);
@@ -63,46 +69,42 @@ export default class Model extends React.Component {
   onChangeDuration(e) {
     this.setState({
       duration: e.target.value
-    })
+    });
   }
   onChangeStatus(e) {
     this.setState({
       status: e.target.value
-    })
+    });
   }
   onChangeconfigId(e) {
     this.setState({
       configId: e.target.value
-    })
+    });
   }
 
-
   state = {
-
     disabled: true,
-    visible: false,
+    visible: false
   };
-
 
   toggleDisable = () => {
     this.setState({ disabled: !this.state.disabled });
   };
 
   onChange = e => {
-    console.log('checked = ', e.target.checked);
+    console.log("checked = ", e.target.checked);
     this.setState({
-      checked: e.target.checked,
+      checked: e.target.checked
     });
   };
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = e => {
-
     e.preventDefault();
 
     const projectData = {
@@ -113,31 +115,31 @@ export default class Model extends React.Component {
       duration: this.state.duration,
       status: this.state.status,
       configId: this.state.configId
-    }
-    axios.post('http://localhost:8080/project_service/createproject', projectData).then(res => console.log(res.data));
+    };
+    axios
+      .post("http://localhost:8080/project_service/createproject", projectData)
+      .then(res => console.log(res.data));
 
     this.setState({
-
-      projectName: '',
-      type: '',
-      startDate: '',
-      endDate: '',
-      duration: '',
-      status: '',
-      configId: '',
-      visible: false,
+      projectName: "",
+      type: "",
+      startDate: "",
+      endDate: "",
+      duration: "",
+      status: "",
+      configId: "",
+      visible: false
     });
   };
 
   handleCancel = e => {
     console.log(e);
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
   render() {
-
     return (
       <div>
         <Button type="primary" onClick={this.showModal}>
@@ -150,14 +152,13 @@ export default class Model extends React.Component {
           onOk={this.handleOk}
           onCancel={this.handleCancel}
           width="600px"
-
         >
           <Form layout="vertical">
             <Row>
-
               <Col span={24} style={{ padding: "5px" }}>
                 <Form.Item label="Project Name">
-                  <Input placeholder="ProjectName"
+                  <Input
+                    placeholder="ProjectName"
                     value={this.state.projectName}
                     onChange={this.onChangeprojectName}
                   />
@@ -166,38 +167,51 @@ export default class Model extends React.Component {
             </Row>
 
             <Row>
-
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="Type">
-                  <Input placeholder="Type"
+                  <Input
+                    placeholder="Type"
                     value={this.state.type}
                     onChange={this.onChangeType}
                   />
                 </Form.Item>{" "}
               </Col>
 
-
-
-
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="Start Date" style={{ marginBottom: 0 }}>
-
-                  <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}></span>
-                  <Form.Item style={{ display: 'inline-block', width: 'calc(90% - 12px)' }}>
-                    <DatePicker
-                      onChange={this.onChangeStartDate} />
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
+                    <DatePicker onChange={this.onChangeStartDate} />
                   </Form.Item>
                 </Form.Item>
               </Col>
 
-
-
-
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="End Date" style={{ marginBottom: 0 }}>
-
-                  <span style={{ display: 'inline-block', width: '24px', textAlign: 'center' }}></span>
-                  <Form.Item style={{ display: 'inline-block', width: 'calc(90% - 12px)' }}>
+                  <span
+                    style={{
+                      display: "inline-block",
+                      width: "24px",
+                      textAlign: "center"
+                    }}
+                  />
+                  <Form.Item
+                    style={{
+                      display: "inline-block",
+                      width: "calc(90% - 12px)"
+                    }}
+                  >
                     <DatePicker
                       // value={this.state.endDate}
                       onChange={this.onChangeEndDate}
@@ -205,15 +219,13 @@ export default class Model extends React.Component {
                   </Form.Item>
                 </Form.Item>
               </Col>
-
-
             </Row>
 
             <Row>
-
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="Duration">
-                  <Input placeholder="Duration"
+                  <Input
+                    placeholder="Duration"
                     value={this.state.duration}
                     onChange={this.onChangeDuration}
                   />
@@ -222,27 +234,24 @@ export default class Model extends React.Component {
 
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="Status">
-                  <Input placeholder="Status"
+                  <Input
+                    placeholder="Status"
                     value={this.state.status}
                     onChange={this.onChangeStatus}
                   />
                 </Form.Item>{" "}
               </Col>
 
-
               <Col span={8} style={{ padding: "5px" }}>
                 <Form.Item label="Config Id">
-                  <Input placeholder="ConfigId"
+                  <Input
+                    placeholder="ConfigId"
                     value={this.state.configId}
                     onChange={this.onChangeconfigId}
                   />
                 </Form.Item>{" "}
               </Col>
             </Row>
-
-
-
-
 
             {/* <Row>
               <Col>
@@ -276,13 +285,9 @@ export default class Model extends React.Component {
               </Col>
 
             </Row> */}
-
           </Form>
-
         </Modal>
       </div>
     );
   }
 }
-
-
